8936988aa779616a374f47ff3e995d9913eb79d1
qcosocode <carlos.ing.2013@gmail.com>
2021-02-09 23:53:01 -0300

    2 commit

---
+++ src/components/nav/navBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './navBar.css'
+
+function NavBar() {
+    return (
+            <nav>
+                <div className="title-question"><h2>Where in the world?</h2></div>
+                <div className="dark-mode">Dark Mode</div>
+                
+                </nav>
+       
+
+    )
+}
+
+export default NavBar ;
---
+++ src/components/search/search.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Search() {
+    return (
+        <div className="search-filter-container">
+            <div className="search">
+                 <input type="text" id ="texto" placeholder="Search for a country " />
+             </div>
+        <div className="filter"> 
+            <select name="Continent">
+                <option value="1">Africa</option> 
+                <option value="2">América</option> 
+                <option value="3">Europa</option>
+                <option value="4">Asia</option> 
+                <option value="5">Oceania</option> 
+        
+             </select>
+        </div>
+    </div>
+    )
+    
+}
+
+export default Search
